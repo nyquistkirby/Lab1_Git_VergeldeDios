750414122fdf2aef8620430a2ab21be6be44417a
Nyquist Kirby A. Vergel de Dios <nyquistkirby@gmail.com>
2026-09-01 19:06:02 +0800

    Enable login for VergeldeDios branch

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ print("Lab1: Version Control")
 print("Author: Nyquist Kirby A. Vergel de Dios") # <--REPLACE WITH YOUR NAME
 print("Student Number: TUPM-26-1935") # <--REPLACE WITH YOUR STUDENT NUMBER
 print("Class Section: BSECE-1A") # <--REPLACE WITH YOUR CLASS SECTION   
+
+print("Login Feature Enabled")
