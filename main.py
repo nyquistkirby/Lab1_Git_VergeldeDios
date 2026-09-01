58b76b541e54409f88c43907de4e20dac51ad3ef
Nyquist Kirby A. Vergel de Dios <nyquistkirby@gmail.com>
2026-09-01 19:01:38 +0800

    Added section info for Vergel de Dios

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 print("Lab1: Version Control")
 print("Author: Nyquist Kirby A. Vergel de Dios") # <--REPLACE WITH YOUR NAME
 print("Student Number: TUPM-26-1935") # <--REPLACE WITH YOUR STUDENT NUMBER
+
+print("Lab1: Version Control")
+print("Author: Nyquist Kirby A. Vergel de Dios") # <--REPLACE WITH YOUR NAME
+print("Student Number: TUPM-26-1935") # <--REPLACE WITH YOUR STUDENT NUMBER
+print("Class Section: BSECE-1A") # <--REPLACE WITH YOUR CLASS SECTION   
